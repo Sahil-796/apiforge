909d7f66dc248b167f58aa77788391dc0fefdaa9
Sahil-796 <sp0709006@gmail.com>
2025-06-13 23:57:43 +0530

    Made passport.js setup

--- server/index.js
+++ server/index.js
@@ -1,19 +1,45 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const cors  = require('cors')
+require('dotenv').config()
+const mongoose = require('mongoose')
+require('./config/passport')
 
-// Middleware to parse JSON bodies
+app.use(cors({
+  origin: '*',
+  credentials: true
+}));
 app.use(express.json());
 
-// Demo route
-app.get('/', (req, res) => {
-  res.send('Hello, Express!');
-});
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParse: true,
+  useUnifiedTopology: true
+}).then(()=> console.log("MongoDB connected"))
+  .catch(err=> console.log(err)) 
+  
+  
+app.use(session({
+  secret: 'secret123',
+  resave: false,
+  saveUninitiated: false,
+  store: MongoStore.create({ mongoUrl: process.env.MONGO_URI}),
+  cookie: {
+  maxAge: 1000*60*60*24*7, 
+  httpOnly: true,
+  secure: false //change in prod
+}
+}))
 
-// Example POST route
-app.post('/echo', (req, res) => {
-  res.json({ youSent: req.body });
-});
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/', (req,res)=> res.send('Hello developer !'))
+
+const authRoutes = require('./routes/authRoutes');
+app.use('/api/auth', authRoutes);
+
+
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
